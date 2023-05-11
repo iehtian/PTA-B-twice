8190672247e95763abf70150cf288941c2e04a3b
iehtian <1850453937@qq.com>
2023-05-11 18:07:05 +0800

    不写了，吃饭去

---
+++ PAT1027.cpp
@@ -0,0 +1,15 @@
+#include<algorithm>
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    char c;
+    cin >> n >> c;
+    int k = 1;
+    int cont = 1;
+    for (; k<n ; k=2*cont+1)
+    {
+        /* code */
+    }
+    
+}
